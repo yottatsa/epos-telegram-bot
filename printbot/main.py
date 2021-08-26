32d24e150b3b16709689dd9176ec8ec19cffc8d8
Atsuko Ito <me@yottatsa.name>
2021-08-26 14:59:47 +0100

    graphics mode printing

--- printbot/main.py
+++ printbot/main.py
@@ -5,6 +5,7 @@ from tempfile import NamedTemporaryFile
 from typing import Callable, Collection, Dict, Set, Union
 
 from escpos.printer import Dummy, Usb
+from PIL import Image, ImageOps
 from telegram import Update
 from telegram.ext import CallbackContext, MessageHandler, Updater
 from telegram.ext.filters import Filters
@@ -52,14 +53,43 @@ class POSBot:
     @_acl
     def _image(self, update: Update, context: CallbackContext) -> None:
         photo = update.message.photo[-1]
-        imagefile = context.bot.getFile(photo.file_id)
-        logger.info("Received image from %d: %s", update.message.chat_id, imagefile)
-        ext = os.path.splitext(imagefile["file_path"])[1]
+        t_imagefile = context.bot.getFile(photo.file_id)
+        logger.info("Received image from %d: %s", update.message.chat_id, t_imagefile)
+        ext = os.path.splitext(t_imagefile["file_path"])[1]
         with NamedTemporaryFile(suffix=ext) as f:
-            logger.info("Writing to %s", f.name)
-            imagefile.download(f.name)
-            self.printer.image(f.name)
-            self.printer.cut()
+            imagefile = f.name
+            logger.info("Writing to %s", imagefile)
+            t_imagefile.download(imagefile)
+
+            try:
+                profile = self.printer.profile
+                max_width = int(profile.profile_data["media"]["width"]["pixels"])
+                image = Image.open(imagefile)
+                w, h = image.size
+                ar = h / w
+
+                if ar < 1 and h > max_width:
+                    logger.info("Rotating image")
+                    image = image.rotate(90, expand=True)
+                    w, h = image.size
+                    ar = h / w
+
+                size = (max_width, int(max_width * ar))
+                logger.info("Resizing from %s to %s", image.size, size)
+                image.thumbnail(size, Image.ANTIALIAS)
+
+                self.printer.image(image, impl="graphics", center=True)
+                del image
+
+                self.printer.cut()
+                return
+            except KeyError:
+                logger.exception("")
+            except ValueError:
+                logger.exception("")
+            else:
+                self.printer.image(imagefile)
+                self.printer.cut()
 
     def start(self) -> None:
         updater = Updater(self.token)
